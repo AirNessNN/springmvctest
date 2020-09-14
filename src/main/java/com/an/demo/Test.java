4f031b9dfa2db32c50bacc98f594741901b0eb1c
Airness <huhaowei16@hotmail.com>
2020-09-14 16:42:05 +0800

    init

---
+++ src/main/java/com/an/demo/Test.java
@@ -0,0 +1,28 @@
+package com.an.demo;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * @author Airness
+ * @email huhaowei16@hotmail.com
+ * @date 2020/9/14 4:35 下午
+ * @description
+ */
+public class Test {
+
+
+    public void sayHello(String str){
+        System.out.println("你好:" + str);
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Class<?> tc = Class.forName("com.an.demo.Test");
+        Constructor<?> constructor=tc.getConstructor();
+        Object instance = constructor.newInstance();
+
+        Method method = tc.getMethod("sayHello", String.class);
+        method.invoke(instance,"张三");
+    }
+}
